87c2969ae57094630a29fd657ee16aa1f4d248e5
danial999 <danial.ibraheem@gmail.com>
2016-08-01 22:43:31 +0300

    new

--- init2.js
+++ init2.js
@@ -67,14 +67,109 @@ function print(){
 var end = 2009;
  key = localStorage.getItem("Key");
     iter = end -key;
-     for(i=0;(i<dataset[iter].location.length);i++){
-      address = dataset[iter].location[i];
-       
-     
-        console.log("address "+i+" " +address);
+     // 
+    // delay between geocode requests - at the time of writing, 100 miliseconds seems to work well
+    var delay = 100;
+
+
+      // ====== Create map objects ======
+      var infowindow = new google.maps.InfoWindow();
+      var latlng = new google.maps.LatLng(-34.397, 150.644);
+      var mapOptions = {
+        zoom: 8,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      var geo = new google.maps.Geocoder(); 
+      var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+      var bounds = new google.maps.LatLngBounds();
+
+      // ====== Geocoding ======
+      function getAddress(search, next) {
+        geo.geocode({address:search}, function (results,status)
+          { 
+            // If that was successful
+            if (status == google.maps.GeocoderStatus.OK) {
+              // Lets assume that the first marker is the one we want
+              var p = results[0].geometry.location;
+              var lat=p.lat();
+              var lng=p.lng();
+              // Output the data
+                var msg = 'address="' + search + '" lat=' +lat+ ' lng=' +lng+ '(delay='+delay+'ms)<br>';
+                document.getElementById("messages").innerHTML += msg;
+              // Create a marker
+              createMarker(search,lat,lng);
+            }
+            // ====== Decode the error status ======
+            else {
+              // === if we were sending the requests to fast, try this one again and increase the delay
+              if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+                nextAddress--;
+                delay++;
+              } else {
+                var reason="Code "+status;
+                var msg = 'address="' + search + '" error=' +reason+ '(delay='+delay+'ms)<br>';
+                
+              }   
+            }
+            next();
+          }
+        );
+      }
+
+     // ======= Function to create a marker
+     function createMarker(add,lat,lng) {
+       var contentString = add;
+       var marker = new google.maps.Marker({
+         position: new google.maps.LatLng(lat,lng),
+         map: map,
+         zIndex: Math.round(latlng.lat()*-100000)<<5
+       });
+
+      google.maps.event.addListener(marker, 'click', function() {
+         infowindow.setContent(contentString); 
+         infowindow.open(map,marker);
+       });
+
+       bounds.extend(marker.position);
+
      }
 
+      // ======= An array of locations that we want to Geocode ========
+      var addresses = dataset[iter].location;
+
+      // ======= Global variable to remind us what to do next
+      var nextAddress = 0;
+
+      // ======= Function to call the next Geocode operation when the reply comes back
+
+      function theNext() {
+        if (nextAddress < addresses.length) {
+          setTimeout('getAddress("'+addresses[nextAddress]+'",theNext)', delay);
+          nextAddress++;
+        } else {
+          // We're done. Show map bounds
+          map.fitBounds(bounds);
+        }
+      }
+
+      // ======= Call that function for the first time =======
+      theNext();
+
+    // This Javascript is based on code provided by the
+    // Community Church Javascript Team
+    // http://www.bisphamchurch.org.uk/   
+    // http://econym.org.uk/gmap/
+
+    //]]>
+
 }
+
+
+
+
+
+
 }
 
 
